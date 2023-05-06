f9acaeb3716d5d29ebdca752de66d741659e67e9
tbohn2 <tanner.bohn@yahoo.com>
2023-05-06 13:58:43 -0700

    Created route to db.json

--- server.js
+++ server.js
@@ -10,3 +10,11 @@ app.get('/', (req, res) =>
 
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'db/db.json'));
+});
+
+app.listen(PORT, () =>
+    console.log(`Example app listening at http://localhost:${PORT}`)
+);
