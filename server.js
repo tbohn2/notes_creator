8745b54d44cdfcdcb03da13435b65c217eed46c9
tbohn2 <tanner.bohn@yahoo.com>
2023-05-06 13:47:25 -0700

    Created server.js and HTML routes

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3001;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html')));
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/notes.html')));
