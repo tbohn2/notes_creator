c9c812a6ada56684676544d7d820ef8348f77bd1
tbohn2 <tanner.bohn@yahoo.com>
2023-05-06 16:53:53 -0700

    Updated routes to work properly

--- server.js
+++ server.js
@@ -2,19 +2,26 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3001;
+const fs = require('fs');
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/index.html')));
-
-app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/notes.html')));
-
-app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'db/db.json'));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
 });
 
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+app.get('/api/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    return res.json(notes);
+});
+
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html')));
+
 app.listen(PORT, () =>
-    console.log(`Example app listening at http://localhost:${PORT}`)
+    console.log(`Listening at http://localhost:${PORT}`)
 );
